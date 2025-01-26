452a234c725ebb2192ebda78443125647c9eb81a
Emirhan KOLVER <emirhanklvr@gmail.com>
2025-01-26 16:29:19 +0300

    Navigation animations complete

--- app/src/main/java/com/emirhankolver/tmdbapp/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/emirhankolver/tmdbapp/ui/navigation/NavGraph.kt
@@ -1,5 +1,10 @@
 package com.emirhankolver.tmdbapp.ui.navigation
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
@@ -13,11 +18,48 @@ import com.emirhankolver.tmdbapp.ui.populars.PopularsScreen
 import com.google.gson.Gson
 import java.net.URLDecoder
 
+
+private const val ANIM_DURATION = 350
+
 @Composable
 fun NavGraph(navController: NavHostController) {
+
+
     NavHost(
         navController = navController,
-        startDestination = Routes.Home.route
+        startDestination = Routes.Home.route,
+        enterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { +it },
+                animationSpec = tween(durationMillis = ANIM_DURATION)
+            )
+        },
+        exitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { -it },
+                animationSpec = tween(durationMillis = ANIM_DURATION)
+            ) + fadeOut(
+                animationSpec = tween(
+                    ANIM_DURATION, easing = LinearEasing
+                )
+            )
+        },
+        popEnterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { -it },
+                animationSpec = tween(durationMillis = ANIM_DURATION)
+            )
+        },
+        popExitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { +it },
+                animationSpec = tween(durationMillis = ANIM_DURATION)
+            ) + fadeOut(
+                animationSpec = tween(
+                    ANIM_DURATION, easing = LinearEasing
+                )
+            )
+        }
     ) {
         composable(Routes.Home.route) {
             HomeScreen(navController)
